33e172d4d61f1b814bc5fe80e5d2369d08dd275d
Astik Srivastava <101447736+Astik-2002@users.noreply.github.com>
2022-07-17 15:14:23 +0530

    Update mpf_multi_obs.m

--- mpf simulation/mpf_multi_obs.m
+++ mpf simulation/mpf_multi_obs.m
@@ -1,32 +1,25 @@
-function vel_UAV = mpf_multi_obs(x_pos,y_pos,obs,v_rel,a,b,k_rep0,x_goal,y_goal,k_att)
-vx = 0;
-vy = 0;
-for i = 1:length(obs)
-    
-    theta = atan(abs(v_rel(i,2)/v_rel(i,1)));
-    
-    X = x_pos - obs(i,1);
-    Y = y_pos - obs(i,2);
-    
-    z = ((X*cos(theta)+Y*sin(theta))/a)^2+((X*sin(theta)-Y*cos(theta))./b).^2+1;
+function yaw = mpf_multi_obs(x_pos,y_pos,theta,obs,v_ob,b,k_rep0,V,x_goal,y_goal)
+    n = length(obs);
+    a = 2.*b;
+    vx = 0;
+    vy = 0;
+    v_UAV = [V*cos(theta);V*sin(theta)];
+    for i = 1:n
+        v_rel(1) = v_ob(i,1) - v_UAV(1);
+        v_rel(2) = v_ob(i,2) - v_UAV(2);
+        phi = atan(abs(v_rel(2)/v_rel(1)));
+        obs(i,1) = obs(i,1) + v_ob(i,1);
+        obs(i,2) = obs(i,2) + v_ob(i,2);
+        x = x_pos - obs(i,1);
+        y = y_pos - obs(i,2);
+        eta = (x*v_rel(1)+y*v_rel(2))/(sqrt((v_rel(1))^2+(v_rel(2))^2)*sqrt((x)^2+(y)^2));
+        k_rep0 = k_rep0*(sin(pi*eta/2)+1);
+        a(i) = b(i)*(1.73 + eta^2);
+        z = 0.34*((x*cos(phi)+y*sin(phi))/a(i))^2+((x*sin(phi)-y*cos(phi))/b(i))^2+1;
+        vx = vx + 2*k_rep0*(((x*sin(phi))/b(i)^2)-((x*cos(phi))/a(i)^2))/(z^2);
+        vy = vy - 2*k_rep0*(((y*cos(phi))/b(i)^2)+((x*sin(phi))/a(i)^2))/(z^2);
+    end
 
-    phi = (X*cos(theta)+Y*sin(theta))/(sqrt(X^2+Y^2));
-    phi2 = acos(phi)+(pi/3);
-    r = cos(phi2);
-    k_rep = (r^2*(k_rep0-1)+1)*(sqrt((v_rel(i,1))^2+(v_rel(i,2))^2));
-
-    % if(phi < 0.5)
-    %k_rep = k_rep*();
-    vx = vx + 2*k_rep*(((Y*sin(theta))/b^2)-((X*cos(theta))/a^2))/(z^2);
-    vy = vy - 2*k_rep*(((Y*cos(theta))/b^2)+((X*sin(theta))/a^2))/(z^2);
-    % else
-    %     vx = 0;
-    %     vy = 0;
-    % end
-end
-vx = vx + k_att*(x_pos - x_goal);
-vy = vy + k_att*(y_pos - y_goal);
-
-
-vel_UAV = [vx;vy];
+v = [vx;vy] + 0.008.*[x_goal - x_pos; y_goal - y_pos];
+yaw = acos(v(1)/sqrt(v(1)^2+v(2)^2));
 end
